6f2c8a7a8fdf2a30d1049a8e2bbf24b91239d0ed
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-04-30 11:08:23 +0300

    Create Variables2.java

---
+++ Variables2.java
@@ -0,0 +1,27 @@
+public class Variables2 {
+
+	public static void main(String[] args) {
+		
+		// Programkani waxa uu qabanayaa xogta arday
+		
+		String firstName="Mubaarik";
+		String lastName="Maxamed";
+		int age=18;
+		double height=1.75;
+		String course="Introduction to Java";
+		double marks=89.5;
+		char grade='A';
+		
+		
+		System.out.println("Magac 1aad :"+firstName);
+		System.out.println("Magac 2aad :"+lastName);
+		System.out.println("Da'ada :"+age);
+		System.out.println("Dhererka :"+height);
+		System.out.println("Cooraska :"+course);
+		System.out.println("Dhicacha :"+marks);
+		System.out.println("Darajada :"+grade);
+		
+		
+	}
+
+}
