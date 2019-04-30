b6e4ff0f4fea52a2feedaaff65805d3b6d0e931e
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-04-30 11:19:48 +0300

    Create Variables3.java

---
+++ Variables3.java
@@ -0,0 +1,25 @@
+public class Variables3 {
+
+	public static void main(String[] args) {
+		
+		// Programkani waxa uu qabanayaa
+		//alaabata la iibsday
+		//inta mid  ee la iibsaday
+		//kadib waxa uu xisaabinayaa tootalks
+		
+		String productName="Dasani";
+		int quantity=20;
+		double price=0.5;
+		
+		double total=quantity * price;
+		
+		System.out.println("Shayga la iibsaday :"+productName);
+		System.out.println("Tirada la iibsaday :"+quantity);
+		System.out.println("Totalka lacagta    :"+total);
+		
+		
+		
+		
+	}
+
+}
