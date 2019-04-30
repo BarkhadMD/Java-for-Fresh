6eefe9237a60501a6ed74d9bcec3e558bcc0db5d
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-04-30 10:59:04 +0300

    Update and rename Variables.java to Variables1.java

---
+++ Variables1.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		// Programkani waxa uu isku darayaa laba tiro waxa aanu ku siinayaa totalka 
+		
+		int number1=10;
+		int number2=20;
+		
+		System.out.println(number1+ number2);
+	}
+
+}
