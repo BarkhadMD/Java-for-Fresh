ed8cc24440f2e122fd905c8c4e17d8e134ace5ae
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-05-07 08:37:57 +0300

    Create variables5.java

---
+++ variables5.java
@@ -0,0 +1,20 @@
+
+public class Perimeter {
+
+	public static void main(String[] args) {
+		
+		//Programakani waxa uu xisaabinyaa wareega(Perimeterka) rectangle 
+		// wareega reactangle waxaa lagu helahaa wareeg=2(dherer+balac)
+		
+		
+		//varaible declaration and initialization 
+		int dh=20; // dhererka 
+		int b=40; //balaca
+		int wareeg;// pereimter
+		wareeg=2*(dh+b);
+		
+		System.out.println("Waareeg ="+wareeg);
+
+	}
+
+}
