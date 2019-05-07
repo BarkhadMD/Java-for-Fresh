ab4d8163178b6b50452ffe48845ab38c589f11fd
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-05-07 08:37:19 +0300

    Create Variables4.java

---
+++ Variables4.java
@@ -0,0 +1,19 @@
+
+public class AreaRectangle {
+
+	public static void main(String[] args) {
+
+		//Programkani waxa uu xisaabinayaa Area of Rectangle 
+		
+		//variables declaration and initialization 
+		int dherer=14;
+		int balac=20;
+		
+		int area=dherer*balac;
+		
+		System.out.println("Area of Rectangle="+area);
+			
+
+	}
+
+}
