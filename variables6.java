a1475c36febaec065f6a3bcf8941e311135af31d
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-05-07 08:38:42 +0300

    Create variables6.java

---
+++ variables6.java
@@ -0,0 +1,27 @@
+public class Speed {
+
+	public static void main(String[] args) {
+		
+		
+		// Programkani waxa uu xisaabinayaa speedka gaadhi 
+		// Speed ka waxaa lagu helaa (speed=distance/time)
+		// sidaasi daraadeed waxaad u baahantahay seddex varaible
+		
+		// Variable declaration
+		double distance; // masaafada gaadigu socday
+		double time;  // wakhtiga ay ku qaadatay
+		double speed;
+		
+		// variable initialisataion 
+		distance=120.5;
+		time=2.5;
+		
+		speed=distance/time;
+		
+		System.out.println("Speed = "+speed+"km/s");
+		
+		
+
+	}
+
+}
