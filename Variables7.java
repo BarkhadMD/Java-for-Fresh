0e63717ec55531beca670276d7a3e1de11ff8cef
Barkhad Mohamed <barkhad.diiriye@outlook.com>
2019-05-07 08:39:37 +0300

    Create Variables7.java

---
+++ Variables7.java
@@ -0,0 +1,20 @@
+public class Circle {
+
+	public static void main(String[] args) {
+		
+		// programkani waxa uu xisaabinayaa area of circle;
+		// area of circle waxaa laug helaa area= πr2
+		// Waxa aad taqaanaa π waa 3.14
+		//  r squared  waxa ay la mid tahay rxr
+		
+		//variable declaration and initialisation
+		double pi=3.14;
+		double r=15;  // radius of the circle 
+		
+		double area=pi*r*r;
+		
+		System.out.println("Area of Circle="+area);
+	
+	}
+
+}
